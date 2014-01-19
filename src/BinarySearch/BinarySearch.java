bd62144f16b8dfeb10a7096deb2a469339f17e20
Lorne Millwood <artsy.bytes@gmail.com>
2014-01-19 11:20:18 -0800

    Initial Commit
    Binary Search Implementation

---
+++ src/BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+/**
+ * Binary search
+ */
+public class BinarySearch {
+
+    public static int binarySearch(int[] values, int key) {
+        int high = values.length - 1;
+        int low = 0;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midValue = values[mid];
+            if (midValue < key) {
+                low = mid + 1;
+            } else if (midValue > key) {
+                high = mid - 1;
+            } else {
+                //found it
+                return mid;
+            }
+        }
+        // key not found
+        return -1;
+    }
+}
